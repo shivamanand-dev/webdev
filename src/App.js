fe7c4f6a5355ce7b2f0ae444f4dbc776fc7ee812
shivam0anand <shivam_anand@outlook.com>
2020-05-09 01:32:30 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/header";
+import Hero from "./Components/hero/hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }

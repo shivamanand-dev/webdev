8bef4f4f7dc446740dbbefd2b17414b21790c090
shivam0anand <shivam_anand@outlook.com>
2020-05-09 17:08:43 +0530

    modified

--- src/Components/hero/hero.js
+++ src/Components/hero/hero.js
@@ -1,17 +1,33 @@
 import React from "react";
 import classes from "./hero.css";
 import reset from "../../reset.css";
-import bulb from "./../../images/bulb-on.png";
+import blb from "../../images/bulb-on.png";
 
 const hero = () => {
   return (
     <div className={classes.hero}>
       <div className={classes.wrapper + " " + classes.light}>
-        <div className={classes.box}></div>
-        <div className={classes.thread}>
-          <div className={classes.bulb}>
-            <img className={classes.bulbOn} src={bulb} />
+        <div className={classes.tag}>
+          <div className={classes.gridColTwo}>
+            <div className={classes.sloganContainer}>
+              <h2 className={classes.slogan}>
+                we are the one who help you in making your ideas go online
+              </h2>
+            </div>
+            <div className={classes.signUP}></div>
           </div>
+        </div>
+        <div className={classes.box}>
+          <h1 className={classes.logo}>
+            <span className={classes.webdev}>Webdev</span> <br /> tutorials
+          </h1>
+        </div>
+        <div className={classes.bulbContainer}>
+          <button className={classes.bulb}>
+            <div className={classes.bulbImage}>
+              <img className={classes.bolbOn} src={blb} />
+            </div>
+          </button>
         </div>
       </div>
     </div>

378e7c1e3713e60fbe20afb385c0cf0c3972c131
shivam0anand <shivam_anand@outlook.com>
2020-05-11 21:53:07 +0530

    modified

---
+++ src/Components/Courses/courses.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./courses.css";
+import reset from "../../reset.css";
+
+const courses = () => {
+  return (
+    <div>
+      <h2>from courses</h2>
+    </div>
+  );
+};
+
+export default courses;

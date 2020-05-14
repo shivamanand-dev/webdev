95c2b655b1c50d6a4475d6e997be089ef6cd0bc9
shivam0anand <shivam_anand@outlook.com>
2020-05-14 07:54:27 +0530

    modified

--- src/Components/Courses/courses.js
+++ src/Components/Courses/courses.js
@@ -4,9 +4,15 @@ import reset from "../../reset.css";
 
 const courses = () => {
   return (
-    <div>
-      <h2>from courses</h2>
-    </div>
+    <section className={classes.wrapper + " " + classes.courses}>
+      <div class="circles">
+        <div class="circle-multiple">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+        </div>
+      </div>
+    </section>
   );
 };
 

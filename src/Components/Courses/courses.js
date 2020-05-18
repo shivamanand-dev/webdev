b2592a19109b490f51e2355c6d3ed97f378ff601
shivam0anand <shivam_anand@outlook.com>
2020-05-19 00:03:28 +0530

    modified

--- src/Components/Courses/courses.js
+++ src/Components/Courses/courses.js
@@ -4,12 +4,28 @@ import reset from "../../reset.css";
 
 const courses = () => {
   return (
-    <section
-      className={
-        classes.wrapper + " " + classes.courses + " " + classes.gradientBorder
-      }
-    >
+    <section className={classes.wrapper + " " + classes.courses}>
       <h3 className={classes.title}>Courses</h3>
+
+      <article className={classes.gridCol2}>
+        <div className={classes.htmlCircle}>
+          <div className={classes.htmlCircleText}>
+            <h2 className={classes.htmlCircleTitle}>HTML</h2>
+          </div>
+        </div>
+        <div className={classes.about}>
+          <p className={classes.aboutText}>
+            <span className={classes.aboutTextTitle}>
+              HTML Tutorials in Hindi
+            </span>{" "}
+            <br /> The course focuses on HTML basics which is the backbone of
+            any website. The course is suitable for the beginner in web
+            development. It carries an explanation of all the basic elements of
+            HTML in Hindi. The explanation is followed by tasks and solution
+            which leads to building a strong foundation of the subject.
+          </p>
+        </div>
+      </article>
     </section>
   );
 };

5b54217abd2b1b09ba6ff79d21b902b7c76cce57
shivam0anand <shivam_anand@outlook.com>
2020-05-19 00:43:01 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Components/Header/header";
 import Hero from "./Components/hero/hero";
 import Courses from "./Components/Courses/courses";
+import Footer from "./Components/footer/footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <Courses />
+      <Footer />
     </div>
   );
 }
